97f97d12f87b675c5f66fdf1351db4e540fe765a
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-21 20:42:47 +0530

    added report

--- events.js/main.js
+++ events.js/main.js
@@ -1,5 +1,6 @@
 const { Message, Client } = require("discord.js");
 
+let alreadyBeingReminded = [];
 module.exports = {
   name: "messageCreate",
   once: false,
@@ -9,9 +10,26 @@ module.exports = {
    * @param {Client} client
    */
   async execute(message, client) {
-    if (message.content.toLowerCase().startsWith("n m")) {
+    if (
+      message.content.toLowerCase().startsWith("n m") &&
+      !alreadyBeingReminded.includes(`${message.author.id}.m`)
+    ) {
+      alreadyBeingReminded.push(`${message.author.id}.m`);
       await sleep(60000);
-      message.reply("You can run the Mission command again!");
+      message.reply("You can run the **Mission** command again!");
+      alreadyBeingReminded = alreadyBeingReminded.filter(
+        (value) => value !== `${message.author.id}.m`
+      );
+    } else if (
+      message.content.toLocaleLowerCase().startsWith("n r") &&
+      !alreadyBeingReminded.includes(`${message.author.id}.r`)
+    ) {
+      alreadyBeingReminded.push(`${message.author.id}.r`);
+      await sleep(600000);
+      message.reply("You can run the **Report** command again!");
+      alreadyBeingReminded = alreadyBeingReminded.filter(
+        (val) => val !== `${message.author.id}.r`
+      );
     }
   },
 };
