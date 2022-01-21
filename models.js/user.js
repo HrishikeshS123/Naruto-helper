60238c7f1e3324fb038af2bd458cec29e66b1cde
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-21 21:17:30 +0530

    Added mongoose

---
+++ models.js/user.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  userId: String,
+  disabled: {
+    mission: { default: false, type: Boolean },
+    report: { default: false, type: Boolean },
+    challenge: { default: false, type: Boolean },
+    tower: { default: false, type: Boolean },
+    adventure: { default: false, type: Boolean },
+  },
+  stats: {
+    reminders: {
+      mission: { default: 0, type: Number },
+      report: { default: 0, type: Number },
+      challenge: { default: 0, type: Number },
+    },
+  },
+  reminders: {
+    tower: Number,
+    adventure: Number,
+  },
+});
+
+module.exports = new mongoose.model("user", UserSchema, "user");
