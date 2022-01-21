350b137a471b08fb2ee77a39ec78f64ea11edca4
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-21 15:10:45 +0530

    added command handler

--- index.js
+++ index.js
@@ -1,14 +1,105 @@
-const { Client, Intents } = require("discord.js")
+const { Client, Intents, Collection, MessageEmbed } = require("discord.js");
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS]
-})
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+});
+const fs = require("fs");
+
+client.commands = {
+  prefix: "nh ",
+  commands: new Collection(),
+  cooldowns: new Collection(),
+};
+
+// Command Handling
+
+const { cooldowns } = client.commands;
+const commandFolders = fs.readdirSync("./commands");
+
+for (const folder of commandFolders) {
+  const commandFile = fs
+    .readdirSync(`./commands/${folder}`)
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFile) {
+    const command = require(`./commands/${folder}/${file}`);
+    client.commands.commands.set(command.name, command);
+  }
+}
+
+// Command Handling
 
 client.on("ready", () => {
-    console.log("Ready!")
-})
+  console.log("Ready!");
+});
 
 client.on("messageCreate", async (message) => {
-    console.log(message.content)
-})
+  if (!message.content.toLowerCase().startsWith(config.prefix)) return;
+  if (message.author.bot) return;
 
-client.login(process.env.token)
+  const args = message.content
+    .slice(client.commands.prefix.length)
+    .trim()
+    .split(/ +/g);
+  const commandName = args.shift().toLowerCase();
+
+  const command =
+    client.commands.commands.get(commandName) ||
+    client.commands.commands.find(
+      (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+    );
+
+  if (!command) return;
+
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Collection());
+  }
+
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 0) * 1000;
+
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply({
+        embed: new MessageEmbed()
+          .setDescription(
+            `:x: You have to wait for **${Math.ceil(
+              timeLeft
+            )} seconds** before running this command again!`
+          )
+          .setColor("RED"),
+      });
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+  if (command.args && !args.length) {
+    let reply = "You did not provide any arguments!";
+
+    if (command.usage) {
+      reply += `\nThe proper usage would be: \`${client.commands.prefix}${command.name} ${command.usage}\``;
+    }
+    return message.reply({
+      content: reply,
+    });
+  }
+
+  try {
+    command.execute(message, args, client);
+  } catch (e) {
+    console.log(e);
+
+    message.reply("There was an error while executing this command!");
+  }
+});
+
+client.login(process.env.token);
