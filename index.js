9ff0ef2e8cdf59f3e4ecae810ec06bb809ae6d80
HrishikeshS123 <56496992+HrishikeshS123@users.noreply.github.com>
2022-01-23 15:43:43 +0000

    created server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new Client({
 });
 const fs = require("fs");
 const mongoose = require("mongoose");
-require("http").createServer().listen();
+require("http").createServer((req, res) => res.end(process.version)).listen();
 mongoose.connect(process.env.mongo_path);
 client.commands = {
   prefix: "nh ",
