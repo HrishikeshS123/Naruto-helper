43e014e17cf15bd9a7c3f40e5e556e2ccd1f93fc
HrishikeshS123 <rushisalokhe456@gmail.com>
2022-01-23 14:23:43 +0530

    adds stat

--- events/main.js
+++ events/main.js
@@ -15,23 +15,23 @@ module.exports = {
       !alreadyBeingReminded.includes(`${message.author.id}.m`)
     ) {
       alreadyBeingReminded.push(`${message.author.id}.m`);
+      addStat(message.author.id, "m");
       await sleep(60000);
       message.reply("You can run the **Mission** command again!");
       alreadyBeingReminded = alreadyBeingReminded.filter(
         (value) => value !== `${message.author.id}.m`
       );
-      addStat(message.author.id, "m");
     } else if (
       message.content.toLocaleLowerCase().startsWith("n r") &&
       !alreadyBeingReminded.includes(`${message.author.id}.r`)
     ) {
       alreadyBeingReminded.push(`${message.author.id}.r`);
+      addStat(message.author.id, "r");
       await sleep(600000);
       message.reply("You can run the **Report** command again!");
       alreadyBeingReminded = alreadyBeingReminded.filter(
         (val) => val !== `${message.author.id}.r`
       );
-      addStat(message.author.id, "r");
     }
   },
 };
